1a88884b67b142424e257d48014418c25f9442f8
marianomariscal86 <marianomariscal@gmail.com>
2026-06-25 16:20:47 -0600

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -5,6 +5,6 @@
 //  - Clave "anon public" (NO la service_role, esa es secreta)
 // =====================================================================
 window.PURISIMA_CONFIG = {
-  SUPABASE_URL: "https://qgjfsxfynzihudpmpgqo.supabase.co/",
+  SUPABASE_URL: "https://qgjfsxfynzihudpmpgqo.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFnamZzeGZ5bnppaHVkcG1wZ3FvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIzOTU5ODksImV4cCI6MjA5Nzk3MTk4OX0.JAA0FsGwG-t9oINCIKx3n_ZAw5jqMrtdaA8vxEN2se0"
 };
